337a2d6c94dacb657dcc556b643f0c082889f40f
klren0312 <kuanglong0312@sina.com>
2018-04-14 15:58:43 +0800

    实时直播页面，添加评论框

--- src/pages/RealLive/index.js
+++ src/pages/RealLive/index.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import Reflv from 'reflv'
-
+import {Input} from 'antd'
+import './index.less'
 class RealLive extends React.Component{
+  componentDidMount() {
+    console.log(this.props.match.params.id)
+  }
+
   render() {
+    
     return (
-      <Reflv  
-        url={`http://42.56.90.219:8000/live/test.flv`}
-        type="flv"
-        isLive
-        no-cors
-      />
+      <div className="live">
+        <Reflv  
+          url={`http://42.56.90.219:8000/live/test.flv`}
+          type="flv"
+          isLive
+          no-cors
+          style={{width:"60%"}}
+        />
+        <div className="live-text">
+          <div className="live-text__content"></div>
+          <div className="live-text__input">
+            <Input  placeholder="请输入您的评论" />
+          </div>
+        </div>
+      </div> 
     )
   }
 }
